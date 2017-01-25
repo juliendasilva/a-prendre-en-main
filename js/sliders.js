19485fb11c69e0c8803a8b1cc25c93613adf69e2
Leinad93160 <danieldd.leinad@gmail.com>
2017-01-25 12:29:55 +0100

    up

---
+++ js/sliders.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $('.teams__images').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.teams__text'
+    });
+    $('.teams__text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.teams__images',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true
+    });
+});
