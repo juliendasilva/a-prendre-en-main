06c4669b176a6c10800fc7f39c639ad6ac3fb89a
juliendasilva <julien.da-silva@hetic.net>
2017-01-27 10:56:30 +0100

    change image when scroll input range

--- js/inputRange.js
+++ js/inputRange.js
@@ -1,7 +1,33 @@
 var slider = new Slider('#ex1', {
   formatter: function(value) {
-    var text = value * (value / 10);
+    var text                                           = value * (value / 10);
     document.getElementById("donationValue").innerHTML = Math.floor(text) + ' €';
+
+    if (value < 25) {
+      $("#femme1").show();
+      $("#femme2").hide();
+      $("#femme3").hide();
+      $("#femme4").hide();
+    }
+    else if (value < 50){
+      $("#femme1").hide();
+      $("#femme2").show();
+      $("#femme3").hide();
+      $("#femme4").hide();
+    }
+    else if (value < 75){
+      $("#femme1").hide();
+      $("#femme2").hide();
+      $("#femme3").show();
+      $("#femme4").hide();
+    }
+    else{
+      $("#femme1").hide();
+      $("#femme2").hide();
+      $("#femme3").hide();
+      $("#femme4").show();
+    }
+
     return 'Current value: ' + value;
   }
 });
