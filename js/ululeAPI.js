b4e8edf015f4594efd84f8e2237db3d0bee81ccd
juliendasilva <julien.da-silva@hetic.net>
2017-01-27 09:50:50 +0100

    use config.json for apiURL

--- js/ululeAPI.js
+++ js/ululeAPI.js
@@ -1,10 +1,14 @@
-$.ajax({
-  url: "https://api.ulule.com/v1/projects/10-days-10-songs",
-  context: document.body
-}).done(function(json) {
-  console.log(json);
-  var commited = json.amount_raised;
-  var goal = json.goal;
-  document.getElementById("ulule-commited").innerHTML = commited;
-  document.getElementById("ulule-goal").innerHTML = goal;
+$.getJSON( "./js/config.json", function( data ) {
+var urlAPI = data.apiURL;
+  $.ajax({
+    url: urlAPI,
+    context: document.body
+  }).done(function(json) {
+    console.log(json);
+    var commited = json.amount_raised;
+    var goal = json.goal;
+    document.getElementById("ulule-commited").innerHTML = commited;
+    document.getElementById("ulule-goal").innerHTML = goal;
+  });
+
 });
